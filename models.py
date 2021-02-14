78a184f25fbb9f0f89070308eb9a44f156127962
Will <molina.wilfredo.j@gmail.com>
2021-02-13 21:31:26 -0600

    init commit

---
+++ models.py
@@ -0,0 +1,8 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    name = db.Column(db.String)
